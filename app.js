81dae0394e0235f61858ee2b1656d574d1d945e7
Philip Lemaster <philiplemaster@yahoo.com>
2020-04-20 21:56:57 -0400

    roles and departments

--- app.js
+++ app.js
@@ -9,9 +9,10 @@ var connection = mysql.createConnection({
     password: 'root64',
     database: 'employeeDB'
 });
-  
+ 
 connection.connect(function(err){
     if(err) throw err;
+    console.log('Running...')
     runProgram();
 });
 
@@ -27,7 +28,7 @@ function askInput() {
                 type: 'list',
                 name: 'crud',
                 message: 'What would you like to do?',
-                choices: ['View all employees', 'Create a new employee', 'Update an employee', 'Delete an employee']
+                choices: ['View all employees', 'Create a new employee', 'Delete an employee', 'Create a department', 'View all departments', 'Delete a department', 'View employee roles', 'Create an employee role', 'Delete an employee role']
             }
         ])
 
@@ -40,19 +41,39 @@ function askInput() {
                 writeEmployee();
             }
 
-            else if (response.crud == 'Update an employee') {
-                updateEmployee();
-            }
+            // else if (response.crud == 'Update an employee') {
+            //     updateEmployee();
+            // }
 
             else if (response.crud == 'Delete an employee') {
                 deleteEmployee();
+            }
+
+            else if (response.crud == 'Create a department') {
+                writeDepartment();
+            }
+
+            else if (response.crud == 'View all departments') {
+                readDepartments();
+            }
+
+            else if (response.crud == 'Delete a department') {
+                deleteDepartment();
+            }
+
+            else if (response.crud == 'View an employee\'s role') {
+                readRole();
+            }
+
+            else if (response.crud == 'Create an employee role') {
+                writeRole();
             }
         })
 }
 
 function readEmployees() {
     connection.query('SELECT * FROM employee', function(err,res){
-      console.log(res);
+      console.table(res);
       connection.end();
     })
 }
@@ -68,12 +89,21 @@ function writeEmployee() {
             {
                 name: 'lastName',
                 message: "What is the employee's last name?"
-            }
+            },
 
+            {
+                name: 'role',
+                message: "What is the employee's role?"
+            },
+
+            {
+                name: 'manager',
+                message: "Who is this employee's manager?"
+            }
         ])
     
         .then(response => {
-            connection.query(`INSERT INTO employee (first_name, last_name, full_name) VALUES ('${response.firstName}','${response.lastName}', '${response.firstName} ${response.lastName}')`, function(err,res){
+            connection.query(`INSERT INTO employee (first_name, last_name, full_name, role, manager) VALUES ('${response.firstName}','${response.lastName}', '${response.firstName} ${response.lastName}', '${response.role}', '${response.manager}')`, function(err,res){
                 console.log(`${response.firstName} ${response.lastName} added!`);
                 connection.end();
             })
@@ -110,3 +140,104 @@ function deleteEmployee() {
     
 }
 
+function readDepartments() {
+    connection.query('SELECT * FROM department', function(err,res){
+      console.table(res);
+      connection.end();
+    })
+}
+
+function writeDepartment() {
+    inquirer
+        .prompt ([
+            {
+                name: 'name',
+                message: "What is the department name?"
+            }
+        ])
+    
+        .then(response => {
+            connection.query(`INSERT INTO department (name) VALUES ('${response.name}')`, function(err,res){
+                console.log(`${response.name} added!`);
+                connection.end();
+            })
+        })
+}
+
+function deleteDepartment() {
+
+    connection.query('SELECT name FROM department', function(err,res){
+        names = [];
+        for (var y = 0; y < res.length; y++) {
+            names.push(`${res[y].name}`);
+        }
+
+        inquirer
+            .prompt ([
+                {
+                    type: 'list',
+                    name: 'deleteDep',
+                    message: 'Which department would you like to delete?',
+                    choices: names
+                }
+
+            ])
+    
+        .then(response => {
+            console.log(response.deleteEmp);
+            connection.query(`DELETE FROM department WHERE name = '${response.deleteDep}'`, function(err,res){
+                console.log(`${response.deleteDep} deleted!`);
+                connection.end();
+            })
+        })
+      })
+    
+}
+
+function readRole() {
+    connection.query('SELECT * FROM role', function(err,res){
+      console.table(res);
+      connection.end();
+    })
+}
+
+function writeRole() {
+
+    connection.query('SELECT id, name FROM department', function(err,res) {
+        departmentsArr = [];
+        let departments = JSON.stringify(res);
+
+        for (var y = 0; y < res.length; y++) {
+            names.push(`${res[y].name}`);
+        }
+        
+        inquirer
+            .prompt ([
+                {
+                    name: 'title',
+                    message: "What is the title of the role?"
+                },
+
+                {
+                    name: 'salary',
+                    message: "What is the salary of this role?"
+                },
+
+                {
+                    type: 'list',
+                    name: 'department',
+                    message: "What department does this role belong to?",
+                    choices: departments
+                }
+            ])
+        
+            .then(response => {
+                connection.query(`INSERT INTO role (title, salary, department_id) VALUES ('${response.title}', '${response.salary}', '${response.department}')`, function(err,res){
+                    console.log(`${response.name} added!`);
+                    connection.end();
+                })
+            })
+
+    })
+    
+}
--- tracker.sql
+++ tracker.sql
@@ -7,6 +7,18 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     full_name VARCHAR(60),
-    role_id INT,
-    manager_id INT
+    role VARCHAR(30),
+    manager VARCHAR(60)
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department VARCHAR(30)
+);
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
 );
